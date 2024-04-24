123607d15ca9d71eb0b74c9e121da62ff392dcb8
didacjaquet <132382451+DidacJ@users.noreply.github.com>
2024-04-24 15:27:16 +0200

    Proves

--- cooking_zoo/multiagent_main.py
+++ cooking_zoo/multiagent_main.py
@@ -1,9 +1,9 @@
-import gym
+from cooking_zoo.environment.multi_agent_gym import GymCookingEnvironment
 
 num_agents = 2
 max_steps = 400
 render = False
-obs_spaces = ["symbolic", "feature_vector"]
+obs_spaces = ["feature_vector", "feature_vector"]
 action_scheme = "scheme3"
 meta_file = "example"
 level = "coexistence_test"
@@ -12,7 +12,7 @@ end_condition_all_dishes = True
 agent_visualization = ["robot", "human"]
 reward_scheme = {"recipe_reward": 20, "max_time_penalty": -5, "recipe_penalty": -40, "recipe_node_reward": 0}
 
-env = gym.envs.make("cooking_zoo:cookingEnvMA-v1", level=level, meta_file=meta_file, num_agents=num_agents,
+env = GymCookingEnvironment(level=level, meta_file=meta_file, num_agents=num_agents,
                     max_steps=max_steps, recipes=recipes, agent_visualization=agent_visualization,
                     obs_spaces=obs_spaces, end_condition_all_dishes=end_condition_all_dishes,
                     action_scheme=action_scheme, render=render, reward_scheme=reward_scheme)
@@ -25,3 +25,4 @@ truncated = [False, False]
 while not all(terminated) or all(truncated):
     actions = [action_space.sample() for idx in range(num_agents)]
     observation, reward, terminated, truncated, info = env.step(actions)
+    print(env.step(actions))
