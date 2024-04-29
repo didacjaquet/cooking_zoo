d0823f8327fa130a027efb9ee9d038a3d791f081
didacjaquet <132382451+DidacJ@users.noreply.github.com>
2024-04-30 00:11:23 +0200

    Training basic agent completed

--- cooking_zoo/prova.py
+++ cooking_zoo/prova.py
@@ -17,7 +17,7 @@ def train(env_fn, steps: int = 10_000, seed: int | None = 0):
 
     # Add black death wrapper so the number of agents stays constant
     # MarkovVectorEnv does not support environments with varying numbers of active agents unless black_death is set to True
-    env = ss.black_death_v3(env)
+    #env = ss.black_death_v3(env)
 
     # Pre-process using SuperSuit
     # visual_observation = not env.unwrapped.vector_state
@@ -38,11 +38,11 @@ def train(env_fn, steps: int = 10_000, seed: int | None = 0):
     model = PPO(
         MlpPolicy,
         env,
-        verbose=3,
+        verbose=0,
         batch_size=256,
     )
 
-    model.learn(total_timesteps=steps)
+    model = model.learn(total_timesteps=steps, progress_bar=True)
 
     model.save(f"{env.unwrapped.metadata.get('name')}_{time.strftime('%Y%m%d-%H%M%S')}")
 
@@ -79,28 +79,27 @@ def eval(env_fn, num_games: int = 100, render_mode: str | None = None):
 
     model = PPO.load(latest_policy)
 
+    rews = {agent: 0 for agent in env.possible_agents}
     rewards = {agent: 0 for agent in env.possible_agents}
 
     # Note: we evaluate here using an AEC environments, to allow for easy A/B testing against random policies
     # For example, we can see here that using a random agent for archer_0 results in less points than the trained agent
     for i in range(num_games):
-        env.reset()
-        env.action_space(env.possible_agents[0]).seed(i)
+        env.reset(seed=i)
+        #env.action_space(env.possible_agents[0]).seed(i)
 
         for agent in env.agent_iter():
             obs, reward, termination, truncation, info = env.last()
 
+            rewards[agent] += reward
             for a in env.agents:
-                rewards[a] += env.rewards[a]
+                rews[a] += env.rewards[a]
 
             if termination or truncation:
                 break
             else:
-                if agent == env.possible_agents[0]:
-                    act = env.action_space(agent).sample()
-                else:
-                    act = model.predict(obs, deterministic=True)[0]
-            env.step(act)
+                act = model.predict(obs)[0]
+                env.step(act)
     env.close()
 
     avg_reward = sum(rewards.values()) / len(rewards.values())
@@ -110,6 +109,14 @@ def eval(env_fn, num_games: int = 100, render_mode: str | None = None):
     print(f"Avg reward: {avg_reward}")
     print("Avg reward per agent, per game: ", avg_reward_per_agent)
     print("Full rewards: ", rewards)
+    print("-------------------------------------------------------------------------------------")
+    avg_rews = sum(rews.values()) / len(rews.values())
+    avg_rews_per_agent = {
+        agent: rews[agent] / num_games for agent in env.possible_agents
+    }
+    print(f"Avg rewsd: {avg_rews}")
+    print("Avg rews per agent, per game: ", avg_rews_per_agent)
+    print("Full rews: ", rews)
     return avg_reward
 
 
@@ -121,10 +128,10 @@ if __name__ == "__main__":
     action_scheme = "scheme1"
     meta_file = "example"
     level = "coexistence_test"
-    recipes = ["TomatoLettuceSalad", "CarrotBanana"]
+    recipes = ["TomatoSalad", "CarrotBanana"]
     end_condition_all_dishes = True
     agent_visualization = ["robot", "human"]
-    reward_scheme = {"recipe_reward": 20, "max_time_penalty": -5, "recipe_penalty": -40, "recipe_node_reward": 0}
+    reward_scheme = {"recipe_reward": 40, "max_time_penalty": -5, "recipe_penalty": -20, "recipe_node_reward": 0}
 
     env = env(level=level, meta_file=meta_file, num_agents=num_agents,
                                 max_steps=max_steps, recipes=recipes, agent_visualization=agent_visualization,
@@ -134,10 +141,10 @@ if __name__ == "__main__":
     # Set vector_state to false in order to use visual observations (significantly longer training time)
     #env = aec_to_parallel(env)
     # Train a model (takes ~5 minutes on a laptop CPU)
-    train(env, steps=81_920, seed=0)
+    train(env, steps=1000000, seed=0)
 
     # Evaluate 10 games (takes ~10 seconds on a laptop CPU)
-    eval(env, num_games=10, render_mode=None)
+    eval(env, num_games=100, render_mode=None)
 
     # Watch 2 games (takes ~10 seconds on a laptop CPU)
-    eval(env, num_games=2, render_mode="human")
+    eval(env, num_games=20, render_mode="human")
